1d0b1d50df810751b2ed8282be1ac4687f9f0203
Nagendra J <>
2023-09-22 15:27:07 +0530

    first commit.

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Welcome extends Component {
+    render() {
+        return (
+            <>
+                <h3>Hello this is the example of the class compoenent & another component</h3>
+            </>
+        )
+    }
+}
+export default Welcome;
